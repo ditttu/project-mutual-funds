425194c15ed0cb5f10b07fde996829ca2aa2af44
ditttu <adityagulatiadityagulati@gmail.com>
2019-10-27 09:04:16 +0530

    changes in navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav'
 
 const Navigatebar = ({changeRoute}) => {
     return (
-        <Nav fill variant="tabs" activeKey="search" onSelect={changeRoute}>
+        <Nav fill variant="tabs" defaultActiveKey="search" onSelect={changeRoute}>
             <Nav.Link eventKey="search">Search</Nav.Link>
             <Nav.Link eventKey="compare">Compare</Nav.Link>
         </Nav>
