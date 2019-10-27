29ddecb7f428f7217f0898c9bc8a7d3da6a40cc9
ditttu <adityagulatiadityagulati@gmail.com>
2019-10-27 09:08:51 +0530

    changes in navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 // import './navbar.styles.scss'
 import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
 const Navigatebar = ({changeRoute}) => {
     return (
+        <Navbar bg="light" expand="lg">
+        <Navbar.Brand>Cats Stocks</Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
         <Nav fill variant="tabs" defaultActiveKey="search" onSelect={changeRoute}>
             <Nav.Link eventKey="search">Search</Nav.Link>
             <Nav.Link eventKey="compare">Compare</Nav.Link>
         </Nav>
+        </Navbar.Collapse>
+        </Navbar>
     )
 }
 
